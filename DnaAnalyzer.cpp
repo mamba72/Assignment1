f851c5a97783aaacbbaedf04a6078120b43c00a5
Stephen White <saltybanana72@gmail.com>
2019-09-09 21:08:56 -0700

    Added lots of variables and reading file

--- DnaAnalyzer.cpp
+++ DnaAnalyzer.cpp
@@ -14,8 +14,41 @@ int main(int argc, char** argv)
 	//this is the variable 
 	ifstream inputFile;
 
+	//open the file that was provided
 	inputFile.open(argv[1]);
 
+	//check to make sure that we can open the file in the first place
+	if (!inputFile)
+	{
+		cout << "We were unable to open that file.\nTry a different one or close it before asking us to open it.\n";
+		exit(1);
+	}
+
+	string fullFileText;
+	string fileLine;
+	//go through the file getting each line one at a time and adding them to the full string
+	while (std::getline(inputFile, fileLine))
+	{
+		fullFileText = fullFileText + fileLine + "\n";
+	}
+
+	cout << "This is what was read from the file:\n";
+	cout << fullFileText <<endl;
+
+	int numA;
+	int numC;
+	int numG;
+	int numT;
+	int numGC;
+	int numGT;
+	int numGA;
+	int numGG;
+	int numAA;
+	int numAC;
+	int numAG;
+	int numAT;
+	int numCA, numCC, numCG, numCT;
+	int numTA, numTC, numTG, numTT;
 
 
 
