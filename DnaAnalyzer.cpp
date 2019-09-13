ab9dae95d31f3ed7314caf3d26046d9f927b6167
Stephen White <saltybanana72@gmail.com>
2019-09-12 18:14:47 -0700

    Output to file

--- DnaAnalyzer.cpp
+++ DnaAnalyzer.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <fstream>
+#define _USE_MATH_DEFINES //allows me to use the definition of PI from math.h
+#include <math.h>
+#include <time.h>
 using namespace std;
+
+
+//#define M_PI
 
 string ReadFile(string fileName);
 string PrintSummary();
 void CountIndividualChars(string fileText);
 void CountBigrams(string fileText);
+float CalcVariance(string fileName);
+void OutputToFile(string fileName);
+float CalcGaussianDistribution(float mean, float variance);
 
 //my counting variables for keeping track of the amount of each nucliotide and bigram
-int numA = 0, numT, numC = 0, numG = 0;
+int numA = 0, numT = 0, numC = 0, numG = 0;
 int numGC = 0, numGT = 0, numGA = 0, numGG = 0;
 int numAA = 0, numAC = 0, numAG = 0, numAT = 0;
 int numCA = 0, numCC = 0, numCG = 0, numCT = 0;
@@ -16,9 +25,13 @@ int numTA = 0, numTC = 0, numTG = 0, numTT = 0;
 int totalChars = 0;
 int totalBigrams = 0;
 int totalLineLength = 0, totalLineCount = 0;
-int lineLengthMean = 0, lineVariance = 0, lineStdDeviation = 0;
+float lineLengthMean = 0, lineVariance = 0, lineStdDeviation = 0;
+float GaussDistr = 0;
 
 string fullFileText = "";
+
+//this is the variable that will be reading the file
+ifstream inputFile;
 
 int main(int argc, char** argv)
 {
@@ -31,10 +44,8 @@ int main(int argc, char** argv)
 
 	//read the file and get the string of the contents
 	//fullFileText = ReadFile(argv[1]);
-#pragma region Reading the file
 
-	//this is the variable 
-	ifstream inputFile;
+	
 
 	//open the file that was provided
 	inputFile.open(argv[1]);
@@ -67,57 +78,8 @@ int main(int argc, char** argv)
 	}
 
 	inputFile.close();
-
+	//set the text to all lower case
 	fullFileText = lowerFileText;
-
-#pragma endregion
-
-	//print the contents to ensure that the file is correct. (mostly for testing)
-	cout << "This is what was read from the file:\n";
-	cout << fullFileText <<endl;
-
-	//my counting variables for keeping track of the amount of each nucliotide and bigram
-	/*int numA, numT, numC = 0, numG = 0;
-	int numGC, numGT, numGA, numGG;
-	int numAA, numAC, numAG, numAT;
-	int numCA, numCC, numCG, numCT;
-	int numTA, numTC, numTG, numTT;
-	int totalChars = 0;
-	int totalBigrams = 0;
-	int totalLineLength = 0, totalLineCount = 0;*/
-
-	//count the individual letters
-	/*
-	for (char letter : fullFileText)
-	{
-		int curLineLength = 0;
-
-		//ensure that the letter its counting is only a nucleotide. (to avoid the \n i inserted after each line)
-		if (letter == 'a')
-			numA++;
-		else if (letter == 'c')
-			numC++;
-		else if (letter == 'g')
-			numG++;
-		else if (letter == 't')
-			numT++;
-		else if (letter == '\\')//check to see if at the end of the line
-		{
-			totalLineLength += curLineLength;
-			totalLineCount++;
-			curLineLength = 0;
-			continue;
-		}
-		//if the character is not a nucleotide, skip and continue through the loop without counting it
-		else
-			continue;
-
-		curLineLength++;
-		totalChars++;
-	}
-
-	cout << "Finished Counting Chars " << totalChars << "\n";
-	*/
 
 	//check to make sure that there is an even number of characters. if its odd, then skip the last one.
 	if (totalChars % 2 != 0)
@@ -125,70 +87,27 @@ int main(int argc, char** argv)
 		totalChars--;
 	}
 
-	cout << "Full file text: \n" << fullFileText << endl;
-
-	cout << "Ensure Counting Chars is even " << totalChars << "\n";
-
-	//count every pair of nucleotides (bigrams)
-	/*
-	for (int i = 0; i < totalChars; i += 2)
-	{
-		//cout << "I = " << i << endl;
-		string currentPair = " ";
-		currentPair = string(1, fullFileText[i]);
-		currentPair += string(1, fullFileText[i + 1]);
-
-		cout << "Current Pair: " << currentPair << endl;
-
-		if (currentPair == "aa")
-			numAA++;
-		else if (currentPair == "ac")
-			numAC++;
-		else if (currentPair == "ag")
-			numAG++;
-		else if (currentPair == "at")
-			numAT++;
-		else if (currentPair == "ca")
-			numCA++;
-		else if (currentPair == "cc")
-			numCC++;
-		else if (currentPair == "cg")
-			numCG++;
-		else if (currentPair == "ct")
-			numCT++;
-		else if (currentPair == "ga")
-			numGA++;
-		else if (currentPair == "gc")
-			numGC++;
-		else if (currentPair == "gg")
-			numGG++;
-		else if (currentPair == "gt")
-			numGT++;
-		else if (currentPair == "ta")
-			numTA++;
-		else if (currentPair == "tc")
-			numTC++;
-		else if (currentPair == "tg")
-			numTG++;
-		else if (currentPair == "tt")
-			numTT++;
-		else
-			continue;
-
-		totalBigrams++;
-		//i += 2;
-	}
-	*/
-
 
 	//calculate the mean length of each string
 	lineLengthMean = totalChars / totalLineCount;
 
+	//total line length = total chars
+	totalLineLength = totalChars;
 
+	//calculate the variance of the line lengths
+	lineVariance = CalcVariance(argv[1]);
 	
+	//calculate gaussian / normal distribution
+	GaussDistr = CalcGaussianDistribution(lineLengthMean, lineVariance);
+
+	cout << "GaussDistr: " << GaussDistr << endl;
 
 	//print everything
-	PrintSummary();
+	//PrintSummary();
+
+	OutputToFile("StephenWhite.txt");
+
+	return 0;
 }
 
 //FUNCTIONS:
@@ -240,8 +159,6 @@ void WriteToFile()
 //prints the summary of the statistics
 string PrintSummary()
 {
-	//string outputString = "";
-	//build the output string
 	cout << "Total Number of characters: " << totalChars << endl;
 	cout << "Total number of bigrams: " << totalBigrams <<endl;
 	cout << "Total number of lines: " << totalLineCount << endl;
@@ -257,11 +174,13 @@ string PrintSummary()
 	cout << "Total number of TA's: " << numTA << "\tTC's: " << numTC << "\tTG's: " << numTG << "\tTT's: " << numTT << endl;
 
 	cout << "Average line length: " << lineLengthMean << endl;
+	cout << "Line length variance: " << lineVariance << endl;
+	cout << "Standard deviatoin: " << lineStdDeviation << endl;
 
 	//finally print it
 
 	cout << "Done\n";
-	exit(0);
+	
 }
 
 //counts the individual chars of the given string
@@ -294,8 +213,6 @@ void CountIndividualChars(string fileText)
 		curLineLength++;
 		totalChars++;
 	}
-
-	cout << "Finished Counting Chars " << totalChars << "\n";
 }
 
 void CountBigrams(string fileText)
@@ -350,3 +267,94 @@ void CountBigrams(string fileText)
 	}
 }
 
+//this function will return the variance of the length of each line.
+float CalcVariance(string fileName)
+{
+	//open up the file again
+	inputFile.open(fileName);
+
+	//get the sum of (line length - average line length) ^ 2
+	float differenceSum = 0;
+	string line;
+	while (getline(inputFile, line))
+	{
+		differenceSum += pow(line.length() - lineLengthMean, 2);
+	}
+	inputFile.close();
+
+	//divide by the number of lines - 1
+	float variance = differenceSum / (totalLineCount - 1);
+
+	//set standard deviation equal to the square root of variance
+	lineStdDeviation = sqrt(variance);
+
+	return variance;
+}
+
+//calculate the gaussian / normal distribution from the given hint
+float CalcGaussianDistribution(float mean, float variance)
+{
+	srand(time(NULL));
+
+	float a = (float)rand() / (float)RAND_MAX;
+	float b = (float)rand() / (float)RAND_MAX;
+
+	float C = sqrt(-2 * log(a)) * cosf(2 * b * M_PI);
+
+	float D = sqrt(variance) * C + mean;
+
+	return D;
+
+}
+
+//Write all the stuffs to file
+void OutputToFile(string fileName)
+{
+	ofstream outFile;
+	outFile.open(fileName);
+
+	outFile << "Stephen White\n002323381\nData Structures Section 1\nAssignment 1: C++ Review" << endl <<endl;
+
+	outFile << "Total Number of characters: " << totalChars << endl;
+	outFile << "Total number of bigrams: " << totalBigrams << endl;
+	outFile << "Total number of lines: " << totalLineCount << endl;
+	outFile << "Total length of lines: " << totalLineLength << endl;
+	outFile << "Total number of A\'s: " << numA;
+	outFile << "\tC\'s: " << numC;
+	outFile << "\tG\'s:" << numG;
+	outFile << "\tT\'s:" << numT << endl;
+
+	outFile << "Total number of AA's: " << numAA << "\tAC's: " << numAC << "\tAG's: " << numAG << "\tAT's: " << numAT << endl;
+	outFile << "Total number of CA's: " << numCA << "\tCC's: " << numCC << "\tCG's: " << numCG << "\tCT's: " << numCT << endl;
+	outFile << "Total number of GA's: " << numGA << "\tGC's: " << numGC << "\tGG's: " << numGG << "\tGT's: " << numGT << endl;
+	outFile << "Total number of TA's: " << numTA << "\tTC's: " << numTC << "\tTG's: " << numTG << "\tTT's: " << numTT << endl;
+
+	outFile << "Average line length: " << lineLengthMean << endl;
+	outFile << "Line length variance: " << lineVariance << endl;
+	outFile << "Standard deviatoin: " << lineStdDeviation << endl;
+
+	outFile << "Probability of A: " << ((float)numA / (float)totalChars) << "\tC:" << ((float)numC / (float)totalChars) << "\tG:" << ((float)numG / (float)totalChars) << "\tT:" << ((float)numT / (float)totalChars) << endl;
+	outFile << "Probability of AA: " << ((float)numAA / (float)totalChars) << "\tAC:" << ((float)numAC / (float)totalChars) << "\tAG:" << ((float)numAG / (float)totalChars) << "\tAT:" << ((float)numAT / (float)totalChars) << endl;
+	outFile << "Probability of CA: " << ((float)numCA / (float)totalChars) << "\tCC:" << ((float)numCC / (float)totalChars) << "\tCG:" << ((float)numCG / (float)totalChars) << "\tCT:" << ((float)numCT / (float)totalChars) << endl;
+	outFile << "Probability of GA: " << ((float)numGA / (float)totalChars) << "\tGC:" << ((float)numGC / (float)totalChars) << "\tGG:" << ((float)numGG / (float)totalChars) << "\tGT:" << ((float)numGT / (float)totalChars) << endl;
+	outFile << "Probability of TA: " << ((float)numTA / (float)totalChars) << "\tTC:" << ((float)numTC / (float)totalChars) << "\tTG:" << ((float)numTG / (float)totalChars) << "\tTT:" << ((float)numTT / (float)totalChars) << endl;
+
+	outFile << endl << endl;
+
+	//write the 1000 lines of stuff
+	for (int i = 0; i < 10000; ++i)
+	{
+		float gauss = CalcGaussianDistribution(lineLengthMean, lineVariance);
+		string line = "";
+		for (int index = 0; index < gauss; ++i)
+		{
+
+		}
+	}
+
+
+
+	outFile.flush();
+	outFile.close();
+	
+}
--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Sources:
 1. I got my random DNA sequences from this website https://www.bioinformatics.org/sms2/random_dna.html
 2. http://www.fredosaurus.com/notes-cpp/io/readtextfile.html
 3. http://www.cplusplus.com/forum/general/179626/
-4. 
+4. https://www.wikihow.com/Calculate-Variance
+5. http://www.cplusplus.com/doc/tutorial/files/
+6. https://stackoverflow.com/questions/1727881/how-to-use-the-pi-constant-in-c
+7. https://stackoverflow.com/questions/9459035/why-does-rand-yield-the-same-sequence-of-numbers-on-every-run
+8. 
